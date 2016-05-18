f249fa3c87075eb0466897dd81d4d8b5e77100f7
caixt <caixt@broada.com>
2016-05-18 18:11:00 +0800

    测试

---
+++ Mybatis/src/test/java/com/github/cxt/Mybatis/main/SampleTest.java
@@ -0,0 +1,30 @@
+package com.github.cxt.Mybatis.main;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.cxt.Mybatis.dao.UserDao;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/spring-context.xml")
+@Transactional
+@TransactionConfiguration(defaultRollback = true)
+public class SampleTest {
+	
+	@Autowired
+	private UserDao userDao;
+	
+	@Test
+	public void test(){
+		System.out.println(userDao.selectAll().size());
+		
+	}
+
+}
