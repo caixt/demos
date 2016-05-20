92a8288babd2cc0f7ee5145a1cb6137b9b4b3825
caixt <caixt@broada.com>
2016-05-20 17:57:17 +0800

    ssl

--- MyWebEmbedded/src/main/java/com/github/cxt/MyWebEmbedded/JettyMain.java
+++ MyWebEmbedded/src/main/java/com/github/cxt/MyWebEmbedded/JettyMain.java
@@ -12,6 +12,19 @@ public class JettyMain
 	private static final WebAppContext webContext = new WebAppContext();
 
 	public static void main(String[] args) {
+		//./jre/bin/keytool -validity 365 -genkey -alias uyun -keyalg EC -keystore /opt/uyun.keystore -dname "CN=collector.uyun.cn,OU=broada,O=broada,L=Hangzhou,ST=Hangzhou,c=cn" -storepass uyuncollector -keypass uyuncollector
+
+//		SslContextFactory sslContextFactory = new SslContextFactory();
+//		sslContextFactory.setKeyStorePath("uyun.boltdog.keystore");
+//		sslContextFactory.setKeyStorePassword("uyuncollector");
+//		ServerConnector httpsConnector = new ServerConnector(server,
+//		        new SslConnectionFactory(sslContextFactory,"http/1.1"),
+//		        new HttpConnectionFactory());
+//		httpsConnector.setPort(7000);
+//		
+//		server.addConnector(httpsConnector);
+		
+		
 		webContext.setContextPath("/");
 		webContext.setDescriptor("WebRoot/WEB-INF/web.xml");
 		webContext.setParentLoaderPriority(true);
