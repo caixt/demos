c09ca2da5cf2b9c20b84868328a3ecbe4e1c00e1
caixt <caixt@broada.com>
2016-06-28 13:24:38 +0800

    log调整

--- Mylogs/main/src/main/java/com/github/cxt/main/Main.java
+++ Mylogs/main/src/main/java/com/github/cxt/main/Main.java
@@ -1,16 +1,39 @@
 package com.github.cxt.main;
 
+
+import org.slf4j.bridge.SLF4JBridgeHandler;
 import com.github.cxt.log4j.Log4jMain;
 import com.github.cxt.sl4j.Sl4jMain;
 
 public class Main {
+	
+	private static final org.apache.commons.logging.Log commonsLog = org.apache.commons.logging.LogFactory.getLog(Main.class);  
+    private static final org.apache.log4j.Logger log4jLogger = org.apache.log4j.Logger.getLogger(Main.class);  
+    private static final java.util.logging.Logger jdkLogger = java.util.logging.Logger.getLogger(Main.class.getName());  
+    private static final org.slf4j.Logger slf4jLogger = org.slf4j.LoggerFactory.getLogger(Main.class);  
+    private static final org.jboss.logging.Logger jbossLog = org.jboss.logging.Logger.getLogger(Main.class);
+    
+    
 
 	public static void main(String[] args) {
+		initLong();
+		
 		Log4jMain.test();
 		Sl4jMain.test();
 		
-		org.jboss.logging.Logger log = org.jboss.logging.Logger.getLogger(Main.class);
-		log.info("test");
+		commonsLog.info("commonslog");
+		log4jLogger.info("log4jLogger");
+		jdkLogger.info("jdkLogger");
+		slf4jLogger.info("slf4jLogger");
+		jbossLog.info("jbossLog");
+		
+		
+	}
+	
+	
+	private static void initLong(){
+		SLF4JBridgeHandler.removeHandlersForRootLogger();  
+		SLF4JBridgeHandler.install();
 	}
 
 }
