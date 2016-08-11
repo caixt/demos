3e67528c763807d094e28a39e0367e308ce4bed6
caixt <caixt@broada.com>
2016-08-11 11:46:21 +0800

    调整

---
+++ Myjersey/jerseymain/src/main/java/com/github/cxt/Myjersey/main/Demo.java
@@ -0,0 +1,10 @@
+package com.github.cxt.Myjersey.main;
+
+
+//@Profile("test")
+public class Demo {
+
+	public Demo(){
+		System.out.println("--------------------------------------");
+	}
+}
--- Myjersey/jerseymain/src/test/java/com/github/cxt/Myjersey/main/JettyServerTest.java
+++ Myjersey/jerseymain/src/test/java/com/github/cxt/Myjersey/main/JettyServerTest.java
@@ -4,6 +4,7 @@ package com.github.cxt.Myjersey.main;
 public class JettyServerTest {
 
 	public static void main(String[] args) {
+		//System.setProperty("spring.profiles.active", "test");
 		JettyServer.main(new String[]{});
 	}
 	
