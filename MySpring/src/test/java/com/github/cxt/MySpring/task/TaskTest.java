4856234888d91b997505a4181c904be8de9c8d4e
caixt <caixt@broada.com>
2016-09-08 13:32:27 +0800

    调整

--- MySpring/src/test/java/com/github/cxt/MySpring/task/TaskTest.java
+++ MySpring/src/test/java/com/github/cxt/MySpring/task/TaskTest.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TaskTest {
 
 	
+	@SuppressWarnings({ "unused", "resource" })
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/github/cxt/MySpring/task/spring-task-content.xml");
 
