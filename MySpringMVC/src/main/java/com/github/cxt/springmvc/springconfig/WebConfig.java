5d3864d0fd5964721ba2f5be41df6179ef588495
caixt <caixt@broada.com>
2017-03-09 21:58:58 +0800

    增加spring mvc

---
+++ MySpringMVC/src/main/java/com/github/cxt/springmvc/springconfig/WebConfig.java
@@ -0,0 +1,40 @@
+package com.github.cxt.springmvc.springconfig;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.github.cxt.springmvc")
+@ImportResource("classpath:/spring-context.xml")
+public class WebConfig {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/view/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver(){
+		return new SessionLocaleResolver();
+	}
+}
+	
