07813bf1db12a28bed89f854d0aedfd53cdc5e60
caixt <caixt@broada.com>
2017-03-13 20:03:04 +0800

    加入I18N

---
+++ MySpring/src/test/java/com/github/cxt/MySpring/I18N/Main.java
@@ -0,0 +1,37 @@
+package com.github.cxt.MySpring.I18N;
+
+import java.util.Date;
+import java.util.Locale;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { Main.Context.class})
+public class Main {
+
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Test
+	public void test(){
+		System.out.println(messageSource.getMessage("welcome-msg", new Object[]{"aaa", new Date()}, Locale.CHINA));
+	}
+	
+	
+	
+	public static class Context {
+		@Bean
+		public MessageSource messageSource() {
+			ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+			messageSource.setBasename("messages");
+			return messageSource;
+		}
+	}
+}
