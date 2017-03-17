facfa45d76cdd66a21f1f59fa8f2006e183e91fa
caixt <caixt@broada.com>
2017-03-17 17:45:35 +0800

    加入json提交

--- MySpringMVC/src/main/java/com/github/cxt/springmvc/controller/FormController.java
+++ MySpringMVC/src/main/java/com/github/cxt/springmvc/controller/FormController.java
@@ -1,11 +1,14 @@
 package com.github.cxt.springmvc.controller;
 
+import java.io.UnsupportedEncodingException;
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.github.cxt.springmvc.form.Subscriber;
 
 @Controller
@@ -26,5 +29,17 @@ public class FormController {
 		
 		m.addAttribute("message", "Successfully saved person: " + subscriber.toString());
 		return "formPage";
+	}
+	
+	
+	@RequestMapping(value="/json", method=RequestMethod.POST)
+	@ResponseBody
+	public Object test_test(@RequestBody @Valid Subscriber subscriber, BindingResult result) throws UnsupportedEncodingException {
+		if(result.hasErrors()) {
+			return "{\"result\":\"" + result.toString() + "\"}";
+		}
+		System.out.println(subscriber.toString());
+	    return "{\"result\":\"" + subscriber.toString() + "\"}";
+
 	}
 }
