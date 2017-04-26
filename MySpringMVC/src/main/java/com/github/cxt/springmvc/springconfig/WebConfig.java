ea0ed405a24fc410214d04e7f269228106c39cc0
caixt <caixt@broada.com>
2017-04-26 10:05:21 +0800

    调整

--- MySpringMVC/src/main/java/com/github/cxt/springmvc/springconfig/WebConfig.java
+++ MySpringMVC/src/main/java/com/github/cxt/springmvc/springconfig/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+//import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
