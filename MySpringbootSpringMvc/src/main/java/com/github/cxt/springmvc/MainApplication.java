db9e602183ccfbd5a1459287046b736af227b5f6
caixt <caixt@broada.com>
2017-09-07 17:18:31 +0800

    补充

--- MySpringbootSpringMvc/src/main/java/com/github/cxt/springmvc/MainApplication.java
+++ MySpringbootSpringMvc/src/main/java/com/github/cxt/springmvc/MainApplication.java
@@ -13,7 +13,16 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @Configuration
+@EnableSwagger2
 @SpringBootApplication
 @ComponentScan(basePackages="com.github.cxt.springmvc")
 public class MainApplication extends WebMvcConfigurerAdapter {
@@ -38,4 +47,23 @@ public class MainApplication extends WebMvcConfigurerAdapter {
 	public LocaleResolver localeResolver() {
 		return new SessionLocaleResolver();
 	}
+	
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.github.cxt.springmvc.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("springboot利用swagger构建api文档")
+                .description("简单优雅的restfun风格，http://xxxxx/forezp")
+                .termsOfServiceUrl("http://xxxxx/forezp")
+                .version("1.0")
+                .build();
+    }
 }
--- MySpringbootSpringMvc/src/main/java/com/github/cxt/springmvc/controller/FormController.java
+++ MySpringbootSpringMvc/src/main/java/com/github/cxt/springmvc/controller/FormController.java
@@ -1,6 +1,7 @@
 package com.github.cxt.springmvc.controller;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,13 +9,16 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.github.cxt.springmvc.form.Subscriber;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 
 @Controller
 public class FormController {
 	
-
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String loadFormPage(Model m) {
 		m.addAttribute("subscriber", new Subscriber());
@@ -41,5 +45,19 @@ public class FormController {
 		System.out.println(subscriber.toString());
 	    return "{\"result\":\"" + subscriber.toString() + "\"}";
 
+	}
+	
+	
+	@ApiOperation(value="根据名称获取实体", httpMethod="GET", notes="get user by id", response= Subscriber.class)  
+	@RequestMapping(value="/swagger", method=RequestMethod.GET)
+	@ResponseBody
+	public Subscriber swagger(@ApiParam(required=true,value="用户名称",name="name")@RequestParam(value="name")String name) {
+		Subscriber subscriber = new Subscriber();
+		subscriber.setAge(1);
+		subscriber.setBirthday(new Date());
+		subscriber.setEmail("aaaa@email");
+		subscriber.setName(name);
+		subscriber.setReceiveNewsletter(true);
+		return subscriber;
 	}
 }
