2e701f104e09cc1a16e77d56d2751008e64cd664
caixt <caixt@broada.com>
2018-04-13 15:04:37 +0800

    补充

--- MyWebEmbedded/src/main/java/com/github/cxt/MyWebEmbedded/JettyMain.java
+++ MyWebEmbedded/src/main/java/com/github/cxt/MyWebEmbedded/JettyMain.java
@@ -1,6 +1,12 @@
 package com.github.cxt.MyWebEmbedded;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 /**
@@ -9,17 +15,29 @@ import org.eclipse.jetty.webapp.WebAppContext;
  */
 public class JettyMain 
 {
-	private static final WebAppContext webContext = new WebAppContext();
 
 	public static void main(String[] args) {
 
+		WebAppContext webContext = new WebAppContext();
 		webContext.setContextPath("/");
-//		webContext.setDescriptor("WebRoot/WEB-INF/web.xml");
-//		webContext.setParentLoaderPriority(true);
+		webContext.setDescriptor("WebRoot/WEB-INF/web.xml");
+		webContext.setParentLoaderPriority(true);
+		webContext.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "false");
 		webContext.setResourceBase("WebRoot");
-		Server server = new Server(8080);
-		server.setHandler(webContext);
+		Server server = new Server();
+		server.setStopAtShutdown(true);
+		
+		HandlerCollection handlers = new HandlerCollection();
+		handlers.setHandlers(new Handler[] { webContext });
+		server.setHandler(handlers);
+		
 		try {
+			HttpConfiguration http_config = new HttpConfiguration();
+	        http_config.setSendServerVersion(false);
+	        http_config.setSendDateHeader(false);
+			ServerConnector connector = new ServerConnector(server, new HttpConnectionFactory(http_config));
+		    connector.setPort(8080);
+		    server.setConnectors(new Connector[]{connector});
 			server.start();
 			server.join();
 		} catch (Exception e) {
