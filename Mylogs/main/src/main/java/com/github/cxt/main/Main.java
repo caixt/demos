c95dfe3300282f88b859cb83b42b02454de5821d
caixt <caixt@broada.com>
2019-11-18 19:18:34 +0800

    日志补充lo4j2

--- Mylogs/main/src/main/java/com/github/cxt/main/Main.java
+++ Mylogs/main/src/main/java/com/github/cxt/main/Main.java
@@ -9,6 +9,7 @@ public class Main {
 	
 	private static final org.apache.commons.logging.Log commonsLog = org.apache.commons.logging.LogFactory.getLog(Main.class);  
     private static final org.apache.log4j.Logger log4jLogger = org.apache.log4j.Logger.getLogger(Main.class);  
+    private static final org.apache.logging.log4j.Logger log4j2Logger = org.apache.logging.log4j.LogManager.getLogger(Main.class);  
     private static final java.util.logging.Logger jdkLogger = java.util.logging.Logger.getLogger(Main.class.getName());  
     private static final org.slf4j.Logger slf4jLogger = org.slf4j.LoggerFactory.getLogger(Main.class);  
     private static final org.jboss.logging.Logger jbossLog = org.jboss.logging.Logger.getLogger(Main.class);
@@ -23,6 +24,7 @@ public class Main {
 		
 		commonsLog.info("commonslog");
 		log4jLogger.info("log4jLogger");
+		log4j2Logger.info("log4j2Logger");
 		jdkLogger.info("jdkLogger");
 		slf4jLogger.info("slf4jLogger");
 		jbossLog.info("jbossLog");
