8263888891c15dad65126f7c6d1bff17340d88d4
caixt <caixt@broada.com>
2023-10-18 19:46:09 +0800

    补充

--- Mybatis/src/test/java/com/github/cxt/Mybatis/main/BatchTest.java
+++ Mybatis/src/test/java/com/github/cxt/Mybatis/main/BatchTest.java
@@ -1,72 +1,41 @@
 package com.github.cxt.Mybatis.main;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
+import com.github.cxt.Mybatis.Service.UserService;
 
-import com.github.cxt.Mybatis.dao.UserDao;
-import com.github.cxt.Mybatis.entity.User;
-
-//整体来说test2的性能最快
+//jdbc的配置里必须加rewriteBatchedStatements=true  不然test1和test3都会慢
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/spring-context.xml")
-@Transactional
-@TransactionConfiguration(defaultRollback = false)
+//@Transactional
+//@TransactionConfiguration(defaultRollback = false)
 public class BatchTest {
 	@Autowired
-	private JdbcTemplate jdbc;
-	@Autowired
-	private SqlSessionTemplate sqlSessionTemplate;
-	@Autowired
-	private UserDao userDao;
-
-	//执行快，但事务提交慢
+	private UserService userService;
+	
 	@Test
 	public void test1(){
-		UserDao testBatch = sqlSessionTemplate.getMapper(UserDao.class);
-		User user = null;
-		for(int i = 0; i < 100000; i++){
-			user = new User();
-			user.setName("aaa");
-			user.setUserType(1);
-			testBatch.insert(user);
-		}
+		long start = System.currentTimeMillis();
+		userService.test1();
+		System.out.println(System.currentTimeMillis() - start);
 	}
 	
-	//执行慢，但事务提交快,(但sql长度有限制)
+	//(但sql长度有可能限制)
 	@Test
 	public void test2(){
-		List<User> users = new ArrayList<>();
-		User user = null;
-		for(int i = 0; i < 100000; i++){
-			user = new User();
-			user.setName("aaa");
-			user.setUserType(1);
-			users.add(user);
-		}
-		userDao.insertBatch(users);
+		long start = System.currentTimeMillis();
+		userService.test2();
+		System.out.println(System.currentTimeMillis() - start);
 	}
 	
-	//整体性能和test1接近
 	@Test
 	public void test3(){
-		List<Object[]> batchArgs = new ArrayList<>();
-		for(int i = 0; i < 100000; i++){
-			Object[] obj = new Object[2];
-			obj[0] = "aaa";
-			obj[1] = 1;
-			batchArgs.add(obj);
-		}
-		jdbc.batchUpdate("insert into user (name, user_type) values (?,?)", batchArgs);
+		long start = System.currentTimeMillis();
+		userService.test3();
+		System.out.println(System.currentTimeMillis() - start);
 	}
 		
 }
